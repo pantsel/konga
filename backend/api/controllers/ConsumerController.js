c2abdb128fba560468c426fe5778882f81139a8b
pantsel <tselentispanagis@gmail.com>
2016-11-03 20:42:06 +0200

    Used async library for syncing

--- backend/api/controllers/ConsumerController.js
+++ backend/api/controllers/ConsumerController.js
@@ -2,6 +2,7 @@
 
 var _ = require('lodash');
 var unirest = require('unirest')
+var async = require('async')
 
 /**
  * UserController
@@ -13,8 +14,6 @@ module.exports = _.merge(_.cloneDeep(require('../base/Controller')), {
 
     sync : function(req,res) {
 
-        // ToDo using async
-        console.log("sails.config.kong_admin_url",sails.config.kong_admin_url)
         unirest.get(sails.config.kong_admin_url + '/consumers')
             .end(function (response) {
                 if (response.error) return res.kongError(response)
@@ -25,63 +24,51 @@ module.exports = _.merge(_.cloneDeep(require('../base/Controller')), {
                     .exec(function(err,consumers){
                         if (err) return res.negotiate(err)
 
-
+                        // Find the consumers that only exist in Kong's db
                         var onlyInKong = kongConsumers.filter(function(current){
                             return consumers.filter(function(current_b){
                                     return current_b.username == current.username
                                 }).length == 0
                         });
 
-                        console.log("onlyInKong",onlyInKong)
-
+                        // Find the consumers that only exists in Konga's db
                         var onlyInKonga = consumers.filter(function(current){
                             return kongConsumers.filter(function(current_a){
                                     return current_a.username == current.username
                                 }).length == 0
                         });
 
-                        if(onlyInKong.length) {
-                            sails.models.consumer.create(onlyInKong)
-                                .exec(function(err,docs){
-                                    if (err) return res.negotiate(err)
+                        async.series([
+                            function(callback) {
+                                sails.log.info('Importing Kong\'s consumers in Konga]');
 
-                                    // ToDo delete the onlyInKonga records
-
-                                    if(onlyInKonga.length) {
-                                        var ids = onlyInKonga.map(function(item){
-                                            return item.id
-                                        })
-                                        sails.models.consumer.destroy({
-                                            id: ids
-                                        }).exec(function (err){
-                                            if (err) {
-                                                return res.negotiate(err);
-                                            }
-                                            return res.ok();
-                                        });
-                                    }else{
-                                        return res.json(docs)
-                                    }
-
+                                // Add node_id property
+                                onlyInKong.forEach(function(consumer){
+                                    consumer.node_id = req.node_id
                                 })
-                        }else if(onlyInKonga.length) {
-                            var ids = onlyInKonga.map(function(item){
-                                return item.id
-                            })
-                            sails.models.consumer.destroy({
-                                id: ids
-                            }).exec(function (err){
-                                if (err) {
-                                    return res.negotiate(err);
-                                }
-                                return res.ok();
-                            });
-                        }else {
 
+                                sails.models.consumer.create(onlyInKong)
+                                    .exec(function(err,docs){
+                                        if (err) return callback(err)
+                                        return callback()
+                                    })
+                            },
+                            function(callback) {
+                                sails.log.info('Deleting absent consumers from Konga');
+                                var ids = onlyInKonga.map(function(item){
+                                    return item.id
+                                })
+                                sails.models.consumer.destroy({
+                                    id: ids
+                                }).exec(function (err){
+                                    if (err) return callback(err)
+                                    return callback()
+                                });
+                            }
+                        ], function(err) {
+                            if (err) return res.negotiate(err)
                             return res.ok();
-                        }
-
-
+                        });
 
                     })
 
