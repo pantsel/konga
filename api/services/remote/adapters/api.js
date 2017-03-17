cb2fea583f0304cb05d3f100bc9b233a87b41959
Panagis Tselentis <p.tselentis@ime.com.gr>
2017-03-17 16:35:12 +0200

    Added api import adapter

---
+++ api/services/remote/adapters/api.js
@@ -0,0 +1,72 @@
+var unirest = require('unirest')
+
+module.exports = {
+    schema : {
+        "name": "API",
+        "value": "api",
+        "description": "Import Consumers by issuing a GET request to an API",
+        "form_fields": {
+            "connection": {
+                "endpoint": {
+                    "name": "endpoint",
+                    "required" : true,
+                    "type": "text",
+                    "description": "The API endpoint. ex: <code>http://myapi.io/users?limit=3000</code>"
+                },
+                "headers": {
+                    "name": "headers",
+                    "type": "text",
+                    "description": "A comma separated list of headers to send with the request. ex: <code>Authorization:Bearer some-token-key,Content-type:application/json</code>"
+                }
+            },
+            "consumer": {
+                "data_property" : {
+                    "name": "Data property",
+                    "type": "text",
+                    "description": "If specified, the consumers will be extracted from this resulting object's property."
+                },
+                "username": {
+                    "name": "username field",
+                    "type": "text",
+                    "required": true,
+                    "description": "The resulting object's property that will be used as the consumers <code>username</code>."
+                },
+                "custom_id": {
+                    "name": "custom_id field",
+                    "type": "text",
+                    "required": true,
+                    "description": "The resulting object's property will be used as the consumers <code>custom_id</code>."
+                }
+            }
+        }
+    },
+    methods : {
+        loadConsumers : function(req,res) {
+
+            var headers = {}
+            if(req.param('headers')) {
+                var string = req.param('headers')
+                var split = string.split(",")
+                split.forEach(function(keyVal){
+                    var array = keyVal.split(":")
+                    headers[array[0]] = array[1]
+                })
+
+                console.log("Headers =>",headers)
+            }
+
+            var request = unirest.get(req.param('endpoint'))
+            request.headers(headers)
+            request.end(function (response) {
+                if (response.error)  return res.negotiate(response)
+
+                console.log("response.body",response.body)
+
+                var jsonRes = response.body
+
+                return res.json(req.param('data_property') ? jsonRes[req.param('data_property')] : jsonRes)
+            })
+        }
+    }
+
+}
