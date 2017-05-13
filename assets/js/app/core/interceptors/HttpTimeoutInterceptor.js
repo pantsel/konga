620c31df356029eb4f7cc6f330cd5aa2bae817ba
pantsel <tselentispanagis@gmail.com>
2017-05-13 20:26:41 +0300

    Added HttpTimeoutInterceptor

---
+++ assets/js/app/core/interceptors/HttpTimeoutInterceptor.js
@@ -0,0 +1,21 @@
+/**
+ * Auth interceptor for HTTP and Socket request. This interceptor will add required
+ * JWT (Json Web Token) token to each requests. That token is validated in server side
+ * application.
+ *
+ * @see http://angular-tips.com/blog/2014/05/json-web-tokens-introduction/
+ * @see http://angular-tips.com/blog/2014/05/json-web-tokens-examples/
+ */
+(function() {
+  'use strict';
+
+  angular.module('frontend.core.interceptors')
+      .factory('timeoutHttpIntercept', function ($rootScope, $q) {
+        return {
+          'request': function (config) {
+            config.timeout = 5000;
+            return config;
+          }
+        }
+      });
+}());
