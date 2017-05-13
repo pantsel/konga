d05f84c142780d649c1ac28f5b6916d7332b1367
pantsel <tselentispanagis@gmail.com>
2017-05-13 10:49:00 +0300

    Added consumer object to snapshots

--- api/controllers/SnapshotController.js
+++ api/controllers/SnapshotController.js
@@ -50,52 +50,95 @@ module.exports = _.merge(_.cloneDeep(require('../base/Controller')), {
             async.series(fns,function(err,data){
                 if(err) return res.negotiate(err)
 
-                if(node.kong_version == '0-10-x') {
-                    // Foreach upstream get its targets
-                    var fns = []
-                    result.upstreams.forEach(function(upstream){
-                        fns.push(function(cb){
-                            KongService.listAllCb(req,'/upstreams/' + upstream.id + '/targets',function(err,data){
+
+                // Foreach consumer get it's acls
+                var consumerFns = []
+                result.consumers.forEach(function(consumer){
+                    consumerFns.push(function(cb){
+                        KongService.listAllCb(req,'/consumers/' + consumer.id + '/acls',function(err,data){
+                            if(err) return cb()
+                            console.log(data)
+                            if(!consumer.acls) consumer.acls = []
+                            data.data.forEach(function(item){
+                                consumer.acls.push(item)
+                            })
+
+                            return cb()
+                        })
+                    })
+
+
+                    var credentials = ["basic-auth","key-auth","hmac-auth","jwt","oauth2"]
+                    credentials.forEach(function(credential){
+                        consumerFns.push(function(cb){
+                            KongService.listAllCb(req,'/consumers/' + consumer.id + '/' + credential,function(err,data){
                                 if(err) return cb()
-                                console.log(data.data)
-                                if(!result.upstream_targets) result.upstream_targets = []
+                                console.log(data)
+                                if(!consumer.credentials) consumer.credentials = {}
+                                if(!consumer.credentials[credential]) consumer.credentials[credential] = []
                                 data.data.forEach(function(item){
-                                    result.upstream_targets.push(item)
+                                    consumer.credentials[credential].push(item)
                                 })
 
                                 return cb()
                             })
                         })
                     })
+                })
+
+                async.series(consumerFns,function(err,data) {
+                    if (err) return res.negotiate(err)
+
+                    if(node.kong_version == '0-10-x') {
+                        // Foreach upstream get its targets
+                        var fns = []
+                        result.upstreams.forEach(function(upstream){
+                            fns.push(function(cb){
+                                KongService.listAllCb(req,'/upstreams/' + upstream.id + '/targets',function(err,data){
+                                    if(err) return cb()
+                                    console.log(data.data)
+                                    if(!result.upstream_targets) result.upstream_targets = []
+                                    data.data.forEach(function(item){
+                                        result.upstream_targets.push(item)
+                                    })
+
+                                    return cb()
+                                })
+                            })
+                        })
 
 
 
-                    async.series(fns,function(err,data){
-                        if(err) return res.negotiate(err)
+                        async.series(fns,function(err,data){
+                            if(err) return res.negotiate(err)
 
 
+                            sails.models.snapshot.create({
+                                name : req.param("name"),
+                                kong_node_name :  node.name,
+                                kong_node_url : node.kong_admin_url,
+                                kong_version : node.kong_version,
+                                data : result
+                            }).exec(function(err,created){
+                                if(err) return res.negotiate(err)
+                                return res.json(created)
+                            })
+                        })
+                    }else{
                         sails.models.snapshot.create({
                             name : req.param("name"),
                             kong_node_name :  node.name,
-                            kong_node_url : node.kong_admin_url,
                             kong_version : node.kong_version,
                             data : result
                         }).exec(function(err,created){
                             if(err) return res.negotiate(err)
                             return res.json(created)
                         })
-                    })
-                }else{
-                    sails.models.snapshot.create({
-                        name : req.param("name"),
-                        kong_node_name :  node.name,
-                        kong_version : node.kong_version,
-                        data : result
-                    }).exec(function(err,created){
-                        if(err) return res.negotiate(err)
-                        return res.json(created)
-                    })
-                }
+                    }
+                })
+
+
+
             });
 
         })
@@ -124,6 +167,26 @@ module.exports = _.merge(_.cloneDeep(require('../base/Controller')), {
                 //if(key != 'upstream_targets' && key != 'upstreams') {
                     snapshot.data[key].forEach(function(item){
                         fns.push(function(cb){
+
+                            // For consumers, we need to import their acls and credentials as well
+
+                            var consumerAcls = []
+                            var consumerCredentials = []
+
+                            if(key == "consumers") {
+
+                                // Clean up the consumer object, by storing acls and credentials in different variables
+                                consumerAcls = _.cloneDeep(item.acls)
+                                consumerCredentials = _.cloneDeep(item.credentials)
+
+                                delete item.acls
+                                delete item.credentials
+
+                                console.log("item",item)
+
+                            }
+
+
                             KongService.createFromEndpointCb("/" + key,item,function(err,created){
 
                                 if(!responseData[key]) {
@@ -143,8 +206,56 @@ module.exports = _.merge(_.cloneDeep(require('../base/Controller')), {
                                     }
                                     return cb()
                                 }
-                                responseData[key].imported++
-                                return cb(null,created)
+
+
+                                if(key == 'consumers') {
+                                    var consumerFns = []
+                                    // Import acls
+                                    consumerAcls.forEach(function(acl){
+                                        consumerFns.push(function(cb){
+                                            KongService.createFromEndpointCb("/" + key + "/" + item.id + "/acls",acl,function(err,created){
+
+                                                if(err) {
+                                                    responseData[key].failed.count++
+                                                    if(responseData[key].failed.items.indexOf(item.name) < 0) {
+                                                        responseData[key].failed.items.push(item.name)
+                                                    }
+                                                    return cb()
+                                                }
+                                                return cb()
+
+                                            });
+                                        })
+                                    })
+
+
+                                    // Import credentials
+                                    Object.keys(consumerCredentials).forEach(function(credentialKey){
+
+                                        credentialKey,consumerCredentials[credentialKey].forEach(function(credentialData){
+                                            consumerFns.push(function(cb){
+                                                KongService.createFromEndpointCb("/" + key + "/" + item.id + "/" + credentialKey,credentialData,function(err,created){
+
+                                                    if(err) {
+                                                        responseData[key].failed.count++
+                                                        if(responseData[key].failed.items.indexOf(item.name) < 0) {
+                                                            responseData[key].failed.items.push(item.name)
+                                                        }
+                                                        return cb()
+                                                    }
+                                                    return cb()
+
+                                                });
+                                            })
+                                        })
+
+                                    })
+                                }
+
+                                async.series(consumerFns,function(err,data){
+                                    responseData[key].imported++
+                                    return cb(null,data)
+                                })
                             })
                         })
                     })
