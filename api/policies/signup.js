be00ecb74a79bc72ef52c26ceed8143cfd01eb17
pantsel <tselentispanagis@gmail.com>
2017-05-16 22:59:32 +0300

    Fix signup policy

--- api/policies/signup.js
+++ api/policies/signup.js
@@ -7,7 +7,7 @@ module.exports = function signup(request, response, next) {
   sails.models.settings.find().limit(1)
       .exec(function(err,data){
         if(err) return next(err)
-        if(!data || !data[0] || !data[0].signup_enable) return response.forbidden()
+        if(!data || !data[0] || !data[0].data || !data[0].data.signup_enable) return response.forbidden("forbidden")
         return next()
       })
 };
