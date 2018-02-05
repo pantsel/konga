1289db26e21fa2c8fd0b154db0114a79589a2631
Robert Metcalf <rmetcalf9@googlemail.com>
2018-02-05 16:46:38 +0000

    Fixing bug where code failed when enviroment variable isn't set

--- config/default-user-seed-data.js
+++ config/default-user-seed-data.js
@@ -9,27 +9,32 @@ var fs = require('fs');
  on enviroment variable setting
 */
 function getseedData() {
-  var readEnvVar = process.env.KONGA_SEED_USER_DATA_SOURCE_FILE.trim();
-  if (readEnvVar.length == 0) {
-    readEnvVar = undefined
+  var readEnvVar = process.env.KONGA_SEED_USER_DATA_SOURCE_FILE;
+  if (typeof(readEnvVar) == undefined) {
+    readEnvVar = undefined;
   } else {
-    if (!fs.existsSync(readEnvVar)) {
-      console.log('Could not find KONGA_SEED_USER_DATA_SOURCE_FILE')
-      readEnvVar = undefined
-    }
-    try {
-      var seedUserData = require(readEnvVar);
-      if (typeof(seedUserData) != 'object') {
-        readEnvVar = undefined
-      } else {
-        // We may place other checks on file contents here if required
-        console.log('Sucessfully read in user seed data file');
-      }
-    } catch (e) {
-      console.log(e);
-      console.log('Failed to load KONGA_SEED_USER_DATA_SOURCE_FILE');
-      console.log('Reverting to default user seed');
+    readEnvVar = readEnvVar.trim();
+    if (readEnvVar.length == 0) {
       readEnvVar = undefined;
+    } else {
+      if (!fs.existsSync(readEnvVar)) {
+        console.log('Could not find KONGA_SEED_USER_DATA_SOURCE_FILE');
+        readEnvVar = undefined;
+      }
+      try {
+        var seedUserData = require(readEnvVar);
+        if (typeof(seedUserData) != 'object') {
+          readEnvVar = undefined;
+        } else {
+          // We may place other checks on file contents here if required
+          console.log('Sucessfully read in user seed data file');
+        }
+      } catch (e) {
+        console.log(e);
+        console.log('Failed to load KONGA_SEED_USER_DATA_SOURCE_FILE');
+        console.log('Reverting to default user seed');
+        readEnvVar = undefined;
+      }
     }
   }
   if(typeof(readEnvVar) == 'undefined') {
