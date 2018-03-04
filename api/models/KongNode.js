edc11d35b7ef2890944312e96f0a48c1fa9e4ef3
pantsel <tselentispanagis@gmail.com>
2018-03-04 12:39:01 +0200

    Added netdata_url property

--- api/models/KongNode.js
+++ api/models/KongNode.js
@@ -10,117 +10,122 @@ var HealthCheckEvents = require("../events/node-health-checks")
  * @docs        :: http://sailsjs.org/#!documentation/models
  */
 var defaultModel = _.merge(_.cloneDeep(require('../base/Model')), {
-    tableName: "konga_kong_nodes",
-    autoPK: false,
-    attributes: {
-        id: {
-            type: 'integer',
-            primaryKey: true,
-            unique: true,
-            autoIncrement: true
-        },
-        name: {
-            type: 'string',
-            required: true
-        },
-
-        type: {
-            type: 'string',
-            enum: ['default', 'key_auth', 'jwt'],
-            defaultsTo: 'default'
-        },
-
-        kong_admin_url: {
-            type: 'string',
-            required: true
-        },
-        kong_api_key: {
-            type: 'string',
-            defaultsTo: ''
-        },
-
-        /**
-         * JWT
-         */
-        jwt_algorithm : {
-            type : 'string',
-            enum : ['HS256','RS256'],
-            defaultsTo : 'HS256'
-        },
-
-        jwt_key : {
-            type : 'string'
-        },
-
-        jwt_secret : {
-            type : 'string'
-        },
-
-        kong_version: {
-            type: 'string',
-            required: true,
-            defaultsTo: '0-10-x'
-        },
-        health_checks: {
-            type: 'boolean',
-            defaultsTo: false
-        },
-        health_check_details: {
-            type: 'json'
-        },
-        active: {
-            type: 'boolean',
-            required: true,
-            defaultsTo: false
-        }
+  tableName: "konga_kong_nodes",
+  autoPK: false,
+  attributes: {
+    id: {
+      type: 'integer',
+      primaryKey: true,
+      unique: true,
+      autoIncrement: true
+    },
+    name: {
+      type: 'string',
+      required: true
     },
 
-    afterDestroy: function (values, cb) {
-
-        sails.log("KongNode:afterDestroy:called => ", values);
-
-        // Stop health checks
-        values.forEach(function (node) {
-            HealthCheckEvents.emit('health_checks.stop', node);
-        })
-
-        cb();
-
+    type: {
+      type: 'string',
+      enum: ['default', 'key_auth', 'jwt'],
+      defaultsTo: 'default'
     },
 
-    afterUpdate: function (values, cb) {
-
-        sails.log("KongNode:afterUpdate:called()")
-        sails.log("KongNode:afterUpdate:health_checks", values.health_checks)
-
-        // Manage toggle health checks
-        if (values.health_checks) {
-            // Send event to begin health checks for the updated node
-            sails.log("KongNode:afterUpdate:emit health_checks.start")
-            HealthCheckEvents.emit('health_checks.start', values);
-        } else {
-            // Send event to stop health checks for the updated node
-            sails.log("KongNode:afterUpdate:emit health_checks.stop")
-            HealthCheckEvents.emit('health_checks.stop', values);
-        }
-
-        cb()
+    kong_admin_url: {
+      type: 'string',
+      required: true
     },
-    seedData: [
-        {
-            "name": "default",
-            "kong_admin_url": "http://kong:8001",
-            "active": true
-        }
-    ]
+
+    netdata_url: {
+      type: 'string'
+    },
+
+    kong_api_key: {
+      type: 'string',
+      defaultsTo: ''
+    },
+
+    /**
+     * JWT
+     */
+    jwt_algorithm: {
+      type: 'string',
+      enum: ['HS256', 'RS256'],
+      defaultsTo: 'HS256'
+    },
+
+    jwt_key: {
+      type: 'string'
+    },
+
+    jwt_secret: {
+      type: 'string'
+    },
+
+    kong_version: {
+      type: 'string',
+      required: true,
+      defaultsTo: '0-10-x'
+    },
+    health_checks: {
+      type: 'boolean',
+      defaultsTo: false
+    },
+    health_check_details: {
+      type: 'json'
+    },
+    active: {
+      type: 'boolean',
+      required: true,
+      defaultsTo: false
+    }
+  },
+
+  afterDestroy: function (values, cb) {
+
+    sails.log("KongNode:afterDestroy:called => ", values);
+
+    // Stop health checks
+    values.forEach(function (node) {
+      HealthCheckEvents.emit('health_checks.stop', node);
+    })
+
+    cb();
+
+  },
+
+  afterUpdate: function (values, cb) {
+
+    sails.log("KongNode:afterUpdate:called()")
+    sails.log("KongNode:afterUpdate:health_checks", values.health_checks)
+
+    // Manage toggle health checks
+    if (values.health_checks) {
+      // Send event to begin health checks for the updated node
+      sails.log("KongNode:afterUpdate:emit health_checks.start")
+      HealthCheckEvents.emit('health_checks.start', values);
+    } else {
+      // Send event to stop health checks for the updated node
+      sails.log("KongNode:afterUpdate:emit health_checks.stop")
+      HealthCheckEvents.emit('health_checks.stop', values);
+    }
+
+    cb()
+  },
+  seedData: [
+    {
+      "name": "default",
+      "kong_admin_url": "http://kong:8001",
+      "active": true
+    }
+  ]
 });
 
 
 var mongoModel = function () {
-    var obj = _.cloneDeep(defaultModel)
-    delete obj.autoPK
-    delete obj.attributes.id
-    return obj;
+  var obj = _.cloneDeep(defaultModel)
+  delete obj.autoPK
+  delete obj.attributes.id
+  return obj;
 }
 
 module.exports = sails.config.models.connection == 'mongo' ? mongoModel() : defaultModel
