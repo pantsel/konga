6159e927ec4543d3a59b1679e18af73e2e0f7372
pantsel <tselentispanagis@gmail.com>
2018-06-08 20:52:42 +0300

    Fix create db hook

--- makedb/dbs/mysql.js
+++ makedb/dbs/mysql.js
@@ -6,6 +6,22 @@
 
 var mysql = require("../../node_modules/sails-mysql/node_modules/mysql");
 var dbConf = require("../../config/connections");
+var URL = require('url');
+var _ = require('lodash');
+
+
+function parse(uri) {
+
+  var parsed =  URL.parse(uri);
+
+  return {
+    user: parsed.auth.split(":")[0],
+    password: parsed.auth.split(":")[1] || null,
+    host: parsed.hostname,
+    port: parsed.port,
+    database: parsed.pathname.replace('/', '')
+  }
+}
 
 module.exports = {
     run : function (next) {
@@ -16,16 +32,24 @@ module.exports = {
 
     create : function(next) {
 
-        var connection = mysql.createConnection({
+        var parsedOpts;
+        var url = dbConf.connections.mysql.url;
+        if(url) {
+          parsedOpts = parse(url);
+        }
+
+
+
+        var connection = mysql.createConnection(url ? _.omit(parsedOpts,['database']) : {
             host     : dbConf.connections.mysql.host,
             port     : dbConf.connections.mysql.port,
             user     : dbConf.connections.mysql.user,
             password : dbConf.connections.mysql.password
         });
 
-        console.log("Creating database `" + dbConf.connections.postgres.database + "` if not exists.");
+        console.log("Creating database `" + ( parsedOpts ? parsedOpts.database : dbConf.connections.mysql.database ) + "` if not exists.");
 
-        connection.query('CREATE DATABASE IF NOT EXISTS ' + dbConf.connections.postgres.database, function (error, results, fields) {
+        connection.query('CREATE DATABASE IF NOT EXISTS ' + ( parsedOpts ? parsedOpts.database : dbConf.connections.mysql.database ), function (error, results, fields) {
             if (error) {
                 console.error(error);
                 return next(error);
