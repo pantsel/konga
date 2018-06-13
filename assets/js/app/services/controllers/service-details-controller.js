a6b82f66ab51e323369673e6e2518b15008610aa
pantsel <tselentispanagis@gmail.com>
2018-06-14 00:47:12 +0300

    Monkey patch to temporarily solve issue #219

--- assets/js/app/services/controllers/service-details-controller.js
+++ assets/js/app/services/controllers/service-details-controller.js
@@ -3,61 +3,74 @@
  *
  * Note that this file should only contain controllers and nothing else.
  */
-(function() {
+(function () {
   'use strict';
 
   angular.module('frontend.services')
     .controller('ServiceDetailsController', [
-      '$scope', '$rootScope','$log', '$state','ServiceService','$uibModal','MessageService','SettingsService',
-      function controller($scope,$rootScope, $log, $state, ServiceService, $uibModal,MessageService,SettingsService) {
+      '_', '$scope', '$rootScope', '$log', '$state', 'ServiceService', '$uibModal', 'MessageService', 'SettingsService',
+      function controller(_, $scope, $rootScope, $log, $state, ServiceService, $uibModal, MessageService, SettingsService) {
 
-          var availableFormattedVersion = ServiceService.getLastAvailableFormattedVersion($rootScope.Gateway.version);
-          $scope.settings = SettingsService.getSettings();
-          $scope.tags = [];
-          $scope.partial = 'js/app/services/partials/form-service-' + availableFormattedVersion + '.html?r=' + Date.now();
+        var availableFormattedVersion = ServiceService.getLastAvailableFormattedVersion($rootScope.Gateway.version);
+        $scope.settings = SettingsService.getSettings();
+        $scope.tags = [];
+        $scope.partial = 'js/app/services/partials/form-service-' + availableFormattedVersion + '.html?r=' + Date.now();
 
-          $scope.updateService = function() {
+        // Store the original service in a var so that we can later check if the name is modified.
+        // This is a monkey patch that fixes Kong's API inconsistency when using Cassandra in v0.13.x.
+        // https://github.com/Kong/kong/issues/3534
+        var originalService = angular.copy($scope.service);
 
-              $scope.loading = true
-              
-              // workaround, name field creates constraint violation in v0.13.x
-              // delete $scope.service.name;
-              
-              ServiceService.update($scope.service)
-                  .then(function(res){
-                      $log.debug("Update Service: ",res)
-                      $scope.loading = false
-                      MessageService.success('Service updated successfully!')
-                  }).catch(function(err){
-                      console.log("err",err)
-                  $scope.loading = false
-                  var errors = {}
-                  Object.keys(err.data.body).forEach(function(key){
-                      MessageService.error(key + " : " + err.data.body[key])
-                  })
-                  $scope.errors = errors
-              })
+        $scope.updateService = function () {
 
+          $scope.loading = true
+          var data = angular.copy($scope.service);
+
+          // workaround, name field creates constraint violation in v0.13.x when using Cassandra
+          if (!isKongUsingPostgres() && originalService.name === data.name) {
+            delete data.name;
           }
 
-          $scope.onTagInputKeyPress = function ($event) {
-            if($event.keyCode === 13) {
-              if(!$scope.service.extras) $scope.service.extras = {};
-              if(!$scope.service.extras.tags) $scope.service.extras.tags = [];
-              $scope.service.extras.tags = $scope.service.extras.tags.concat($event.currentTarget.value);
-              $event.currentTarget.value = null;
-            }
-          }
-
-          function getTags() {
-            ServiceService.getTags().then(function (res) {
-              $scope.tags = res.data;
+          ServiceService.update(data)
+            .then(function (res) {
+              console.log("Update Service: ", res)
+              $scope.loading = false
+              MessageService.success('Service updated successfully!');
+              originalService = res.data; // ref. monkey patch
             }).catch(function (err) {
-              console.error(err);
+            console.log("err", err)
+            $scope.loading = false
+            var errors = {}
+            Object.keys(err.data.body).forEach(function (key) {
+              MessageService.error(key + " : " + err.data.body[key])
             })
-          }
+            $scope.errors = errors
+          })
 
-          getTags();
+        }
+
+        $scope.onTagInputKeyPress = function ($event) {
+          if ($event.keyCode === 13) {
+            if (!$scope.service.extras) $scope.service.extras = {};
+            if (!$scope.service.extras.tags) $scope.service.extras.tags = [];
+            $scope.service.extras.tags = $scope.service.extras.tags.concat($event.currentTarget.value);
+            $event.currentTarget.value = null;
+          }
+        }
+
+        function getTags() {
+          ServiceService.getTags().then(function (res) {
+            $scope.tags = res.data;
+          }).catch(function (err) {
+            console.error(err);
+          })
+        }
+
+        function isKongUsingPostgres() {
+          return _.get($rootScope.Gateway, 'configuration.database') === 'postgres';
+        }
+
+        getTags();
 
       }
     ])
