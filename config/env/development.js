67f590cada7abbe86bdb73e9beaec6181701aa3e
pantsel <tselentispanagis@gmail.com>
2018-07-24 15:25:39 +0300

    Update README.md and added SSL env vars

--- config/env/development.js
+++ config/env/development.js
@@ -10,19 +10,27 @@
  * any private information to this file!
  *
  */
+
+var fs = require('fs');
+
 module.exports = {
-    /***************************************************************************
-     * Set the default database connection for models in the development       *
-     * environment (see config/connections.js and config/models.js )           *
-     ***************************************************************************/
+  /***************************************************************************
+   * Set the default database connection for models in the development       *
+   * environment (see config/connections.js and config/models.js )           *
+   ***************************************************************************/
 
-    hookTimeout: process.env.KONGA_HOOK_TIMEOUT || 60000,
+  hookTimeout: process.env.KONGA_HOOK_TIMEOUT || 60000,
 
-    port: process.env.KONGA_BACKEND_PORT || 1337,
+  port: process.env.PORT || 1337,
 
-    kong_admin_url: process.env.KONG_ADMIN_URL || 'http://127.0.0.1:8001',
+  // kong_admin_url: process.env.KONG_ADMIN_URL || 'http://127.0.0.1:8001',
 
-    log: {
-        level: process.env.KONGA_LOG_LEVEL || "debug"
-    }
+  ssl: {
+    key: process.env.SSL_KEY_PATH ? fs.readFileSync(process.env.SSL_KEY_PATH) : null,
+    cert: process.env.SSL_CRT_PATH ? fs.readFileSync(process.env.SSL_CRT_PATH) : null
+  },
+
+  log: {
+    level: process.env.KONGA_LOG_LEVEL || "debug"
+  }
 };
